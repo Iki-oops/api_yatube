67afa5ae7f7d08b15b104378792019125d5433df
Iki <bambagaevdmitrij@gmail.com>
2021-05-20 03:14:28 +0800

    y

--- posts/permissions.py
+++ posts/permissions.py
@@ -3,7 +3,6 @@ from rest_framework import permissions
 
 class CustomPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if (request.method in permissions.SAFE_METHODS or
-            request.user == obj.author):
+        if request.method in permissions.SAFE_METHODS:
             return True
-        return False
+        return request.user == obj.author
