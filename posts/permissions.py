d502c936becd4b1be8c3925400b5c66a679776b5
Iki <bambagaevdmitrij@gmail.com>
2021-05-20 03:01:25 +0800

    y

--- posts/permissions.py
+++ posts/permissions.py
@@ -1,13 +1,9 @@
 from rest_framework import permissions
-from rest_framework.response import Response
-from rest_framework import status
 
 
 class CustomPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS or request.user == obj.author:
+        if (request.method in permissions.SAFE_METHODS or
+            request.user == obj.author):
             return True
         return False
-        # if request.user != obj.author:
-        #     return Response(status=status.HTTP_403_FORBIDDEN)
-        # return True
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import get_object_or_404
 
-from rest_framework import status, viewsets
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.response import Response
 
 from .models import Post
 from .serializers import PostSerializer, CommentSerializer
@@ -10,7 +9,7 @@ from .permissions import CustomPermission
 
 
 class PostViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticatedOrReadOnly&CustomPermission]
+    permission_classes = [IsAuthenticatedOrReadOnly & CustomPermission]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
@@ -20,7 +19,7 @@ class PostViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly&CustomPermission]
+    permission_classes = [IsAuthenticatedOrReadOnly & CustomPermission]
 
     def get_queryset(self):
         post = get_object_or_404(Post, id=self.kwargs['post_id'])
